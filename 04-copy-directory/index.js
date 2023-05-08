dd90eb5c88277ce0f5dc657737686a34b38eb087
p0lluxstar <web-registra@yandex.ru>
2023-05-08 23:39:00 +0300

    feat: task04 / 080523-23:38

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathOldFolder = path.join(__dirname, 'files');
+const pathNewFolder = path.join(__dirname, 'files-copy');
+
+fs.rm(pathNewFolder, { recursive: true, force: true }, (err) => {
+  fs.mkdir(pathNewFolder, { recursive: true }, (err) => {
+    fs.readdir(pathOldFolder, (err, files) => {
+
+      files.forEach((el) => {
+        const pathOldfile = path.join(pathOldFolder, el);
+        const pathNewfile = path.join(pathNewFolder, el);
+        
+        fs.copyFile(pathOldfile , pathNewfile, (err) => {
+        });
+      });
+    });
+  });
+});
